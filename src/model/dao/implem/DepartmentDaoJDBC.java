0f613dae47203cb12a31ea68b7edfb5480609298
Leandro Perego <leperego@hotmail.com>
2022-07-04 14:47:12 -0300

    Criado classes para implementar a interface Dao

---
+++ src/model/dao/implem/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.implem;
+
+import java.util.List;
+
+import model.dao.Dao;
+import model.entidades.Department;
+
+public class DepartmentDaoJDBC implements Dao<Department>{
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/model/dao/implem/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.implem;
+
+import java.util.List;
+
+import model.dao.Dao;
+import model.entidades.Seller;
+
+public class SellerDaoJDBC implements Dao<Seller> {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
