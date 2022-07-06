26a398b064642893ea570c073c72bf54ae16f0ea
Leandro Perego <leperego@hotmail.com>
2022-07-06 20:05:46 -0300

    Implementado metodo update SellerDaoJDBC

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,7 @@
 package aplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import model.dao.Dao;
 import model.dao.DaoFactory;
 import model.dao.DaoSellerJDBC;
-import model.entidades.Department;
 import model.entidades.Seller;
 
 public class Program {
@@ -15,10 +10,9 @@ public class Program {
 		
 		DaoSellerJDBC bancoSeller = DaoFactory.createSellerDao();
 		
-		List<Seller> lista = new ArrayList<Seller>();
-		lista = bancoSeller.findAll();
-		
-		lista.forEach(System.out::println);
+		Seller seller = bancoSeller.findById(1);
+		seller.setName("Mudado Teste");
+		bancoSeller.update(seller);
 		
 	}
 
